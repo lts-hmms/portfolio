b28df4860483b437891b00cc78562420b27364a7
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-04-04 20:51:10 +0200

    feat: scroll to top button

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState} from 'react';
+import { ChevronUp } from 'react-bootstrap-icons';
+
+const ScrollToTop = () => {
+    const [showScrollTopButton, setShowScrollTopButton] = useState(false)
+
+    useEffect(() => {
+      window.addEventListener('scroll', () => {
+        if(window.scrollY > 300) {
+          setShowScrollTopButton(true)
+        } else {
+          setShowScrollTopButton(false)
+        }
+      })
+        // window.addEventListener('scroll', () => {
+        //     window.scrollY > 300 ? setShowScrollTopButton(true) : setShowScrollTopButton(false) 
+        // })
+    }, []);
+
+    const scrollTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      };
+  return (
+    <div>
+    {showScrollTopButton && <ChevronUp onClick={scrollTop} className='top-btn-position top-btn-style'></ChevronUp>}
+    </div>
+  )
+}
+
+export default ScrollToTop
