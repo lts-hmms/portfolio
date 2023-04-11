32512652d682eda5d2958ab26eaf963c270b80de
lts-hmms <97221909+lts-hmms@users.noreply.github.com>
2023-04-11 13:29:11 +0200

    build: downgrade to react17 bec. of switch

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs: ['en', 'de'],
+    fallbackLng: "en",
+    detection: {
+      order: ['cookie', 'htmlTag', 'localStorage', 'path'],
+      caches: ['cookie'],
+    }, 
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: {
+      useSuspense: false
+    }
+  });
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ReactDOM.render(
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
